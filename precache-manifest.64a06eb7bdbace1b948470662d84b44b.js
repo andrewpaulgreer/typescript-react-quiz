be18075a24966423d4bfc39fb783052bb6587192
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-09-22 10:54:37 -0400

    Updates

---
+++ precache-manifest.64a06eb7bdbace1b948470662d84b44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c1bac629e8f8e5f31b5ce3eb6c7f84",
+    "url": "/typescript-react-quiz/index.html"
+  },
+  {
+    "revision": "b0c5ea4ce1d2ad28b484",
+    "url": "/typescript-react-quiz/static/js/2.9c1b7529.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/typescript-react-quiz/static/js/2.9c1b7529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1d81d80cf72e619d2b",
+    "url": "/typescript-react-quiz/static/js/main.115e94fe.chunk.js"
+  },
+  {
+    "revision": "094e6b20f0ed98008697",
+    "url": "/typescript-react-quiz/static/js/runtime-main.bf5c8206.js"
+  },
+  {
+    "revision": "b59ea9b9a061ea047c4b518d70cc25b2",
+    "url": "/typescript-react-quiz/static/media/ts-quiz.b59ea9b9.jpg"
+  }
+]);
